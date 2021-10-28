cfd173352aceefa7ab5c32ca2810ea861ce71adc
adamray <adamrayw@users.noreply.github.com>
2021-10-29 00:37:50 +0700

    Wrap MaterialApp with MultiProvider and add some routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:courseku_mobile/pages/detail_tutorial.dart';
 import 'package:courseku_mobile/pages/home/main_page.dart';
 import 'package:courseku_mobile/pages/splash_page.dart';
 import 'package:courseku_mobile/pages/tutorial_page.dart';
+import 'package:courseku_mobile/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'pages/sign_up_page.dart';
 import 'pages/sign_in_page.dart';
 
@@ -16,16 +20,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/sign-up': (context) => const SignUpPage(),
-        '/sign-in': (context) => const SignInPage(),
-        '/main': (context) => const MainPage(),
-        '/tutorial': (context) => const TutorialPage(),
-        '/detail-tutorial': (context) => const DetailTutorial(),
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/sign-up': (context) => const SignUpPage(),
+          '/sign-in': (context) => const SignInPage(),
+          '/main': (context) => const MainPage(),
+          '/tutorial': (context) => const TutorialPage(),
+          '/detail-tutorial': (context) => const DetailTutorial(),
+        },
+      ),
     );
   }
 }
