f744eb7070b1d858fd6b0332d973cfb42e12788f
adamray <adamrayw@users.noreply.github.com>
2021-10-29 00:35:12 +0700

    change name based logged user

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,13 +1,15 @@
 // ignore_for_file: avoid_unnecessary_containers, unused_element, unused_import, unused_local_variable
 
 import 'dart:convert';
-import 'package:courseku_mobile/models/homeprog_model.dart';
+import 'package:courseku_mobile/models/user_model.dart';
+import 'package:courseku_mobile/providers/auth_provider.dart';
 import 'package:courseku_mobile/theme.dart';
 import 'package:courseku_mobile/widgets/programming_card.dart';
 import 'package:courseku_mobile/widgets/courses_by_field.dart';
 import 'package:flutter/material.dart';
-import 'package:courseku_mobile/services/homeprog_service.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,16 +19,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var homes = <HomeProgModel>[];
   @override
   void initState() {
     super.initState();
   }
 
   final String apiUrl = "http://courseku.herokuapp.com/api/home";
+  final String apiUrl2 = "http://courseku.herokuapp.com/api/field";
 
   Future<List<dynamic>> fetchProg() async {
     final result = await http.get(Uri.parse(apiUrl));
+    return json.decode(result.body);
+  }
+
+  Future<List<dynamic>> fetchField() async {
+    final result = await http.get(Uri.parse(apiUrl2));
     return json.decode(result.body);
   }
 
@@ -34,12 +41,23 @@ class _HomePageState extends State<HomePage> {
     return data['name'];
   }
 
-  String _img(dynamic data_img) {
-    return data_img['img_url'];
+  String _img(dynamic dataImg) {
+    return dataImg['img_url'];
+  }
+
+  String _nameField(dynamic dataField) {
+    return dataField['name'];
+  }
+
+  String _imgField(dynamic dataFImg) {
+    return dataFImg['img_url'];
   }
 
   @override
   Widget build(BuildContext context) {
+    AuthProvider authProvider = Provider.of(context);
+    UserModel user = authProvider.user;
+
     Widget programmingCarousel() {
       return Container(
         height: 80,
@@ -50,7 +68,7 @@ class _HomePageState extends State<HomePage> {
             if (snapshot.hasData) {
               return ListView.builder(
                   scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.all(8),
+                  padding: const EdgeInsets.all(8),
                   itemCount: snapshot.data.length,
                   itemBuilder: (BuildContext context, int index) {
                     return Row(
@@ -94,7 +112,7 @@ class _HomePageState extends State<HomePage> {
                     );
                   });
             } else {
-              return Center(child: CircularProgressIndicator());
+              return const Center(child: CircularProgressIndicator());
             }
           },
         ),
@@ -103,18 +121,61 @@ class _HomePageState extends State<HomePage> {
 
     Widget coursesByField() {
       return Container(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              Row(
-                children: const [
-                  CoursesByFieldCard(),
-                  CoursesByFieldCard(),
-                ],
-              ),
-            ],
-          ),
+        height: 80,
+        width: double.infinity,
+        child: FutureBuilder<List<dynamic>>(
+          future: fetchField(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.all(8),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Row(
+                      children: [
+                        Container(
+                          width: 200,
+                          height: 64,
+                          margin: const EdgeInsets.only(right: 22),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            // color: Colors.red,
+                            border: Border.all(
+                              width: 0.5,
+                              color: secondaryTextColor,
+                            ),
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                          child: Row(
+                            children: [
+                              Image.network(
+                                _imgField(snapshot.data[index]),
+                                width: 40,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                _nameField(snapshot.data[index]),
+                                style: headerTextStyle.copyWith(
+                                  fontSize: 18,
+                                  fontWeight: regular,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  });
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
         ),
       );
     }
@@ -134,11 +195,16 @@ class _HomePageState extends State<HomePage> {
                         width: 40,
                         height: 40,
                         decoration: const BoxDecoration(
+                          color: Color(0xff3B82F6),
                           shape: BoxShape.circle,
-                          image: DecorationImage(
-                            fit: BoxFit.fill,
-                            image:
-                                NetworkImage('https://i.imgur.com/BoN9kdC.png'),
+                        ),
+                        child: Center(
+                          child: Text(
+                            user.name[0],
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
@@ -146,7 +212,7 @@ class _HomePageState extends State<HomePage> {
                         width: 14,
                       ),
                       Text(
-                        'Hi, Adam',
+                        'Hi, ${user.name}',
                         style: primaryTextStyle.copyWith(
                           fontSize: 18,
                           fontWeight: medium,
