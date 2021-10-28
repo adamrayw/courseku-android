7a64320566c1817fb46479256b3e9dc4b8c06573
adamray <adamrayw@users.noreply.github.com>
2021-10-29 00:41:29 +0700

    Integration with register api

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -1,75 +1,165 @@
+import 'package:courseku_mobile/providers/auth_provider.dart';
 import 'package:courseku_mobile/theme.dart';
 import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
-  Widget fullNameInput() {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: 'Full Name',
-        border: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: secondaryTextColor,
-            width: 0.5,
-          ),
-          borderRadius: BorderRadius.circular(6.0),
-        ),
-      ),
-    );
-  }
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
 
-  Widget emailInput() {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: 'Email',
-        border: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: secondaryTextColor,
-            width: 0.5,
-          ),
-          borderRadius: BorderRadius.circular(6.0),
-        ),
-      ),
-    );
-  }
+class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController nameController = TextEditingController(text: '');
 
-  Widget passwordInput() {
-    return TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'Password',
-        border: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: secondaryTextColor,
-            width: 0.5,
-          ),
-          borderRadius: BorderRadius.circular(6.0),
-        ),
-      ),
-    );
-  }
+  TextEditingController emailController = TextEditingController(text: '');
 
-  Widget confirmPasswordInput() {
-    return TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'Confirm Password',
-        border: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: secondaryTextColor,
-            width: 0.5,
-          ),
-          borderRadius: BorderRadius.circular(6.0),
-        ),
-      ),
-    );
-  }
+  TextEditingController passwordController = TextEditingController(text: '');
+
+  TextEditingController passwordConfirmationController =
+      TextEditingController(text: '');
+
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
+    AuthProvider authProvider = Provider.of<AuthProvider>(context);
+
+    handleSignUp() async {
+      setState(() {
+        isLoading = true;
+      });
+      if (await authProvider.register(
+        name: nameController.text,
+        email: emailController.text,
+        password: passwordController.text,
+        passwordConfirmation: passwordConfirmationController.text,
+      )) {
+        Navigator.pushNamed(context, '/main');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: Color(0xff15D2A7),
+            content: Text(
+              'Register Berhasil!',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: Color(0xffff0f0f),
+            content: Text(
+              'Register Gagal!',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        );
+      }
+
+      setState(() {
+        isLoading = false;
+      });
+    }
+
+    Widget fullNameInput() {
+      return TextFormField(
+        controller: nameController,
+        decoration: InputDecoration(
+          hintText: 'Full Name',
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: secondaryTextColor,
+              width: 0.5,
+            ),
+            borderRadius: BorderRadius.circular(6.0),
+          ),
+        ),
+      );
+    }
+
+    Widget emailInput() {
+      return TextFormField(
+        controller: emailController,
+        decoration: InputDecoration(
+          hintText: 'Email',
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: secondaryTextColor,
+              width: 0.5,
+            ),
+            borderRadius: BorderRadius.circular(6.0),
+          ),
+        ),
+      );
+    }
+
+    Widget passwordInput() {
+      return TextFormField(
+        controller: passwordController,
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: secondaryTextColor,
+              width: 0.5,
+            ),
+            borderRadius: BorderRadius.circular(6.0),
+          ),
+        ),
+      );
+    }
+
+    Widget confirmPasswordInput() {
+      return TextField(
+        controller: passwordConfirmationController,
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Confirm Password',
+          border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: secondaryTextColor,
+              width: 0.5,
+            ),
+            borderRadius: BorderRadius.circular(6.0),
+          ),
+        ),
+      );
+    }
+
+    Widget signUpButton() {
+      return Container(
+        height: 48,
+        width: double.infinity,
+        child: TextButton(
+          onPressed: handleSignUp,
+          style: TextButton.styleFrom(
+            backgroundColor: primaryTextColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6.0),
+            ),
+          ),
+          child: isLoading
+              ? const CircularProgressIndicator(
+                  color: Colors.white,
+                  strokeWidth: 2.0,
+                )
+              : Text(
+                  'Sign Up',
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: bold,
+                  ),
+                ),
+        ),
+      );
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Container(
@@ -118,29 +208,7 @@ class SignUpPage extends StatelessWidget {
                 height: 38,
               ),
               // ignore: sized_box_for_whitespace
-              Container(
-                height: 48,
-                width: double.infinity,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/main');
-                  },
-                  style: TextButton.styleFrom(
-                    backgroundColor: primaryTextColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(6.0),
-                    ),
-                  ),
-                  child: Text(
-                    'Sign Up',
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: bold,
-                    ),
-                  ),
-                ),
-              ),
+              signUpButton(),
               const SizedBox(
                 height: 54,
               ),
