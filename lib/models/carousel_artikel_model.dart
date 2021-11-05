c3c67dfb4eaccae6562b3c24fca47201ad330e08
adamray <adamrayw@users.noreply.github.com>
2021-11-06 00:08:21 +0700

    Add new properties for author

--- lib/models/carousel_artikel_model.dart
+++ lib/models/carousel_artikel_model.dart
@@ -1,14 +1,20 @@
 class CarouselArtikelModel {
   late int id;
   late String name;
+  late String author;
   late String slug;
 
-  CarouselArtikelModel(
-      {required this.id, required this.name, required this.slug});
+  CarouselArtikelModel({
+    required this.id,
+    required this.name,
+    required this.author,
+    required this.slug,
+  });
 
   CarouselArtikelModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     name = json['name'];
+    author = json['author'];
     slug = json['slug'];
   }
 
@@ -16,6 +22,7 @@ class CarouselArtikelModel {
     return {
       'id': id,
       'name': name,
+      'author': author,
       'slug': slug,
     };
   }
