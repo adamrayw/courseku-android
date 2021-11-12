c174adc5e482568002fc83cfe96cb5b02d8ebf08
adamray <adamrayw@users.noreply.github.com>
2021-11-12 21:03:32 +0700

    Add detail-course page

---
+++ lib/pages/detail_course.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DetailCourse extends StatelessWidget {
+  const DetailCourse({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text(
+          "Detail tutorial",
+        ),
+      ),
+    );
+  }
+}
