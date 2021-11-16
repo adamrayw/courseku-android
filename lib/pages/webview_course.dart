8a567041a6b7b3c65ee12763268bc8ec6939cf81
adamray <adamrayw@users.noreply.github.com>
2021-11-16 16:16:03 +0700

    Add webview

---
+++ lib/pages/webview_course.dart
@@ -0,0 +1,34 @@
+import 'package:courseku_mobile/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebviewCourse extends StatelessWidget {
+  final String link;
+  const WebviewCourse({Key? key, required this.link}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Icon(
+          Icons.close,
+          color: primaryTextColor,
+        ),
+        title: Text(
+          link,
+          style: primaryTextStyle,
+        ),
+        elevation: 0.4,
+      ),
+      body: Builder(
+        builder: (BuildContext context) {
+          return WebView(
+            initialUrl: link,
+            javascriptMode: JavascriptMode.unrestricted,
+          );
+        },
+      ),
+    );
+  }
+}
