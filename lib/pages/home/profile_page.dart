32a04370f8f6b0582ea70a6292a9a5e0b19168e6
adamray <adamrayw@users.noreply.github.com>
2021-11-17 23:23:55 +0700

    Add edit profile button

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -9,48 +9,102 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: primaryTextColor,
+        // centerTitle: true,
         title: Text(
           'Profile',
           style: headerTextStyle.copyWith(
             color: Colors.white,
-            fontSize: 24,
+            fontSize: 18,
             fontWeight: bold,
           ),
         ),
         elevation: 0,
       ),
-      body: Container(
-        width: double.infinity,
-        height: 180,
-        decoration: BoxDecoration(
-          color: primaryTextColor,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: 40,
-              height: 40,
-              decoration: const BoxDecoration(
-                color: Color(0xff3B82F6),
-                shape: BoxShape.circle,
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 320,
+            decoration: BoxDecoration(
+              color: primaryTextColor,
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(
+                top: 60,
               ),
-              child: Center(
-                child: Text(
-                  "A",
-                  style: primaryTextStyle.copyWith(
-                    fontSize: 20,
-                    color: Colors.white,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                      color: Color(0xffffffff),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "A",
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 48,
+                          color: secondaryTextColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Adam Ray',
+                    style: headerTextStyle.copyWith(
+                      fontSize: 32,
+                      fontWeight: bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 30,
+              vertical: 35,
+            ),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: secondaryTextColor,
+                        width: 0.5,
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Edit Profile",
+                        style: secondaryTextStyle.copyWith(
+                          fontSize: 16,
+                          fontWeight: bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.edit,
+                        color: primaryTextColor,
+                      ),
+                    ],
                   ),
                 ),
-              ),
+              ],
             ),
-            Text(
-              'Adam Ray',
-              style: headerTextStyle,
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
