02b5e8c778a48d170f274c828eeaf7463ed30020
adamray <adamrayw@users.noreply.github.com>
2021-11-17 22:59:13 +0700

    Redesign profile page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:courseku_mobile/theme.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Profile Page'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryTextColor,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.close,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(
+          'Profile',
+          style: headerTextStyle.copyWith(color: Colors.white),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: primaryTextColor,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 40,
+              height: 40,
+              decoration: const BoxDecoration(
+                color: Color(0xff3B82F6),
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Text(
+                  "A",
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'Adam Ray',
+              style: headerTextStyle,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
