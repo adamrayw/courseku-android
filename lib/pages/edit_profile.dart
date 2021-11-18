581ff0fd3e772ba1b74404f4c7cbdeff8b0170ff
adamray <adamrayw@users.noreply.github.com>
2021-11-18 21:40:49 +0700

    now user can edit profile name

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -1,42 +1,67 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:convert';
+
 import 'package:courseku_mobile/models/user_model.dart';
+import 'package:courseku_mobile/pages/home/profile_page.dart';
 import 'package:courseku_mobile/providers/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:courseku_mobile/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class EditProfile extends StatefulWidget {
-  const EditProfile({Key? key}) : super(key: key);
+  final int id;
+  const EditProfile({Key? key, required this.id}) : super(key: key);
 
   @override
   State<EditProfile> createState() => _EditProfileState();
 }
 
 class _EditProfileState extends State<EditProfile> {
-  TextEditingController nameController = TextEditingController(text: '');
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+
+  bool isLoading = false;
+
+  Future handelEdit() async {
+    final response = await http.put(
+        Uri.parse('http://courseku.herokuapp.com/api/update-profile/' +
+            widget.id.toString()),
+        body: {
+          "name": nameController.text,
+        });
+    return json.decode(response.body);
+  }
+
+  Widget nameInput() {
+    return TextFormField(
+      controller: nameController,
+      decoration: InputDecoration(
+        hintText: 'Nama Lengkap',
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: secondaryTextColor,
+            width: 0.5,
+          ),
+          borderRadius: BorderRadius.circular(6.0),
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "Nama tidak boleh kosong!";
+        }
+
+        return null;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     AuthProvider authProvider = Provider.of(context);
     UserModel user = authProvider.user;
-
-    bool isLoading = false;
-
-    Widget nameInput() {
-      return TextField(
-        controller: nameController,
-        decoration: InputDecoration(
-          hintText: 'Nama Lengkap',
-          border: OutlineInputBorder(
-            borderSide: BorderSide(
-              color: secondaryTextColor,
-              width: 0.5,
-            ),
-            borderRadius: BorderRadius.circular(6.0),
-          ),
-        ),
-      );
-    }
 
     Widget emailInput() {
       return TextField(
@@ -72,47 +97,55 @@ class _EditProfileState extends State<EditProfile> {
           vertical: 28,
           horizontal: 28,
         ),
-        child: Column(
-          children: [
-            emailInput(),
-            const SizedBox(
-              height: 18,
-            ),
-            nameInput(),
-            const SizedBox(
-              height: 18,
-            ),
-            Container(
-              height: 48,
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-                  backgroundColor: primaryTextColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6.0),
-                  ),
-                ),
-                child: isLoading
-                    ? Text(
-                        'Logging in...',
-                        style: GoogleFonts.poppins(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    : Text(
-                        'Update',
-                        style: GoogleFonts.poppins(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              emailInput(),
+              const SizedBox(
+                height: 18,
               ),
-            ),
-          ],
+              nameInput(),
+              const SizedBox(
+                height: 18,
+              ),
+              Container(
+                height: 48,
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      handelEdit();
+                      Navigator.pushNamed(context, '/');
+                    }
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: primaryTextColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                  ),
+                  child: isLoading
+                      ? Text(
+                          'Logging in...',
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      : Text(
+                          'Update',
+                          style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
